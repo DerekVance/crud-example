60bbf20e459f0734d5143db8d978857878051355
Derek Vance <devance65@gmail.com>
2015-07-23 14:28:42 -0600

    routes good no css

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var db = require('monk')(process.env.MONGOLAB_URI || process.env.HOST) 
+var db = require('monk')(process.env.MONGOLAB_URI || process.env.HOST)
 var articles = db.get('articles')
+var validate = require('../lib/error')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('article/index', {});
+  articles.find({}, function(err, record){
+  res.render('article/index', {articles: record});
+  })
 });
 
 router.get('/article/new', function(req, res, next){
@@ -13,27 +16,48 @@ router.get('/article/new', function(req, res, next){
 });
 
 router.post('/article/new', function(req, res, next){
+  var title = req.body.title;
+  var urls = req.body.url;
+  var textcolor = req.body.textcolor;
+  var excerpt = req.body.excerpt;
+  var body = req.body.body;
+  var verify = validate(title, excerpt, body)
+    if(verify.length > 0) {
+      res.render('article/new', {error: verify})
+    } else {
   articles.insert({
-    title: req.body.title,
-    url: req.body.url,
-    textcolor: req.body.textcolor,
-    excerpt: req.body.excerpt,
-    body: req.body.body
-  })
-  res.redirect('/')
+    title: title,
+    url: url,
+    textcolor: textcolor,
+    excerpt: excerpt,
+    body: body
+  });
+  res.redirect('/');
+};
 });
-
 router.get('/article/:id', function(req, res, next){
-  res.render('article/show');
+  articles.findOne({_id: req.params.id}, function(err, record){
+      res.render('article/show', {articles: record});
+  });
 });
 
 router.get('/article/:id/edit', function(req, res, next){
-  res.render('article/edit');
+  articles.findOne({_id: req.params.id}, function(err, record){
+  res.render('article/edit', {articles: record});
+});
 });
 
 router.post('/article/:id/edit', function(req, res, next){
+  articles.update({_id: req.params.id},  {$set: {
+    'title': req.body.title,
+    'url': req.body.url,
+    'textcolor': req.body.textcolor,
+    'excerpt': req.body.excerpt,
+    'body': req.body.body
+  }});
   res.redirect('/');
-})
+});
+
 
 router.post('/:id', function(req, res, next){
   res.redirect('/');
