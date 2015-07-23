3dc4ade16ff7f5b5b374f59411b2c6954263f974
Derek Vance <devance65@gmail.com>
2015-07-23 12:09:41 -0600

    routes defined

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+require('dotenv').load()
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -21,6 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var db = require('monk')(process.env.HOST) 
+var articles = db.get('articles')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('article/index', {});
 });
 
+router.get('/article/new', function(req, res, next){
+  res.render('article/new');
+});
+
+router.post('/article/new', function(req, res, next){
+  articles.insert({
+    title: req.body.title,
+    url: req.body.url,
+    textcolor: req.body.textcolor,
+    excerpt: req.body.excerpt,
+    body: req.body.body
+  })
+  res.redirect('/')
+});
+
+router.get('/article/:id', function(req, res, next){
+  res.render('article/show');
+});
+
+router.get('/article/:id/edit', function(req, res, next){
+  res.render('article/edit');
+});
+
+router.post('/article/:id/edit', function(req, res, next){
+  res.redirect('/');
+})
+
+router.post('/:id', function(req, res, next){
+  res.redirect('/');
+})
 module.exports = router;
